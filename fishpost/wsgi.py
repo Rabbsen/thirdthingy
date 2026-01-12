5e98f50d18e5c7f8deccaf9a18a55da736ad7527
Rabbsen <r.b_rasmus@hotmail.com>
2026-01-12 11:07:41 +0100

    added fising post app using django

---
+++ fishpost/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fishpost project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fishpost.settings')
+
+application = get_wsgi_application()
