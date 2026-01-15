0d5e7f86ca7f485ac816287c020f95bd0ff255b7
Rabbsen <r.b_rasmus@hotmail.com>
2026-01-15 17:28:30 +0100

    added post model, media, url, admin

--- fishpost/urls.py
+++ fishpost/urls.py
@@ -21,4 +21,11 @@ from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
+    path("new/", views.create_post, name="create_post"),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# The post model here
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="fish_images/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
