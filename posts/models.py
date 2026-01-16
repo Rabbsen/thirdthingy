fb18b715e440bdd022daa2d847988e1acb7bf2c3
Rabbsen <r.b_rasmus@hotmail.com>
2026-01-16 20:08:17 +0100

    added cards header styling etc

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    length = models.PositiveIntegerField(null=True, blank=True)
     image = models.ImageField(upload_to="fish_images/")
     created_at = models.DateTimeField(auto_now_add=True)
 
