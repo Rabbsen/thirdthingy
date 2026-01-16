f7afcfe9f92a0d56d18732684bf23ab7951783b2
Rabbsen <r.b_rasmus@hotmail.com>
2026-01-16 21:30:10 +0100

    use postgres on heroku via databaseurl

--- fishpost/settings.py
+++ fishpost/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'fishpost.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
@@ -127,3 +131,5 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+
